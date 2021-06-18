7b193db73cf90e592793c4328b020b9cde71cc75
LuaBarbosa <lua.barbosa@yahoo.com.br>
2021-06-18 11:11:56 -0300

    Feat: Criando Models

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  username: "luana_bbf",
+  password: "lu220489",
+  database: "desafio_tec",
+  host: "localhost",
+  port: "3036",
+  dialect: "mysql",
+  define: {
+    timestamps: true,
+  },
+};
